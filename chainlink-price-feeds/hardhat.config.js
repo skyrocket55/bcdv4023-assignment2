ff49afe4df24f491651974186afdb7071ff9b4d1
skyrocket55 <67418626+skyrocket55@users.noreply.github.com>
2024-04-08 10:49:56 -0400

    initial commit

---
+++ chainlink-price-feeds/hardhat.config.js
@@ -0,0 +1,24 @@
+require("@nomicfoundation/hardhat-toolbox");
+// require('@nomiclabs/hardhat-ethers');
+require('dotenv').config();
+
+require("@nomicfoundation/hardhat-ethers");
+require("hardhat-deploy");
+require("hardhat-deploy-ethers");
+
+const {
+  ALCHEMY_API_KEY,
+  METAMASK_PRIVATE_KEY
+} = process.env;
+
+
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+  solidity: "0.8.24",
+  networks: {
+    sepolia: {
+      url: `https://eth-sepolia.g.alchemy.com/v2/${ALCHEMY_API_KEY}`,
+      accounts: [METAMASK_PRIVATE_KEY],
+    },
+  },
+};
